7861c2e3aaac198741388b8ff219ea626ebbd828
Romain Mottier <romain.mottier@outlook.com>
2025-03-25 10:46:02 +0100

    Create coefs_waves.py

---
+++ build/conic_pulse/coefs_waves.py
@@ -0,0 +1,32 @@
+
+import numpy as np
+
+def lame_coefficients_solid(Vp, Vs, rho):
+    mu = rho * Vs**2
+    lambda_ = rho*Vp**2 -2*mu
+    return lambda_, mu
+
+def lame_coefficients_fluid(Vp2, rho):
+    mu = rho * Vp2**2
+    return mu
+
+# Exemple d'utilisation :
+rho_s = 2.6/2.0 # Masse volumique en kg/m^3
+Vp_s  = 4.0/2.0 # Vitesse des ondes P en m/s
+Vs    = 2.0/2.0 # Vitesse des ondes S en m/s
+Vp_f  = 1.0/2.0
+rho_f = 1.0/2.0
+
+lambda_, mu = lame_coefficients_solid(Vp_s, Vs, rho_s)
+print("\n")
+print("SOLIDE:")
+print("mu =", mu)
+print("lambda =", lambda_)
+print("rho =", rho_s)
+print("\n")
+mu = lame_coefficients_fluid(Vp_f, rho_f)
+print("FLUIDE:")
+print("mu =", mu)
+print("rho =", rho_f)
+print("\n")
+
